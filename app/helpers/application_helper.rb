ca1d5c48fc79ce31a7d77e05a6ebb7a67d17108f
Gerard Leijdekkers <gerard@eet.nu>
2013-11-30 13:47:32 +0100

    Added some color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def arbitrage_profit(buy, sell, fee)
-    "#{((sell - buy) / buy * 100 - fee).round(2)}% (b: #{buy} - s:#{sell})"
+    ((sell - buy) / buy * 100 - fee).round(2)
+  end
+  
+  def print_arbitrage_profit(profit, buy, sell)
+    "#{profit}% (b: #{buy} - s:#{sell})"
   end
 end
