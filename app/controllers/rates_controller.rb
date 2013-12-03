c2398aefb620dd0fa308e5475c716b90ad84871a
Gerard Leijdekkers <gerard@eet.nu>
2013-12-03 15:26:44 +0100

    deal with flakey exchanges

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,13 +1,13 @@
 class RatesController < ApplicationController
   def index
     @rates = {}
-    @rates['BTC-e']        = Btc.new
-    @rates['Vircurex']     = Vircurex.new
-    @rates['Bter']         = Bter.new
-    @rates['fxbtc']        = Fxbtc.new
-    @rates['Cryptsy']      = Cryptsy.new
-    @rates['Crypto-trade'] = CryptoTrade.new
-    @rates['Kraken']       = Kraken.new
-    @rates['Coins-E']      = CoinsE.new
+    @rates['BTC-e']        = Btc.new rescue nil
+    @rates['Vircurex']     = Vircurex.new rescue nil
+    @rates['Bter']         = Bter.new rescue nil
+    @rates['fxbtc']        = Fxbtc.new rescue nil
+    @rates['Cryptsy']      = Cryptsy.new rescue nil
+    @rates['Crypto-trade'] = CryptoTrade.new rescue nil
+    @rates['Kraken']       = Kraken.new rescue nil
+    @rates['Coins-E']      = CoinsE.new rescue nil
   end
 end
